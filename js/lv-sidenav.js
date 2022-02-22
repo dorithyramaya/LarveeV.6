10b52eee6a5f30b4319f4fa9903f79893b1d48a7
Dorithy Ramaya <44577076+dorithyramaya@users.noreply.github.com>
2022-02-22 17:15:53 +0800

    Dashboard update, submenu work

--- js/lv-sidenav.js
+++ js/lv-sidenav.js
@@ -20,3 +20,48 @@ menuIconEl.on('click', function() {
 sidenavCloseEl.on('click', function() {
     toggleClassName(sidenavEl, 'active');
 });
+
+//sidenav collapsed when clicked toggle
+    const sidenavToggle = document.getElementById('sidenav-toggle');
+    sidenavToggle.onclick = function () {
+    const sidenav = document.getElementById('sidenav-left');
+    sidenav.classList.toggle('opened');
+    console.log("TOGGLE");
+}
+
+//GO-TO Search
+    const words = [" Abandoned Checkouts","Abandoned Checkouts Settings","Add Blog Post","Add Bulk Voucher","Add Customer","Add EasyParcel Location","Add Featured Product","Add Footer","Add Form","Add Header","Add Navigation","Add Newsletter", "Add Page","Add Page Release","Add Product","Add Role","Add Running Number","Add Staff","Add Store Location"]
+    const containerEl = document.querySelector('.goto-search')
+    const formEl = document.querySelector('#search')
+    const dropEl = document.querySelector('.drop')
+
+    const formHandler = (e) => {
+    const userInput = e.target.value.toLowerCase()
+
+    if(userInput.length === 0) {
+    dropEl.style.height = 0
+    return dropEl.innerHTML = ''
+}
+
+    const filteredWords = words.filter(word => word.toLowerCase().includes(userInput)).sort().splice(0, 5)
+
+    dropEl.innerHTML = ''
+    filteredWords.forEach(item => {
+    const listEl = document.createElement('li')
+    listEl.textContent = item
+    if(item === userInput) {
+    listEl.classList.add('match')
+}
+    dropEl.appendChild(listEl)
+})
+
+    if(dropEl.children[0] === undefined) {
+    return dropEl.style.height = 0
+}
+
+    let totalChildrenHeight = dropEl.children[0].offsetHeight * filteredWords.length
+    dropEl.style.height = totalChildrenHeight + 'px'
+
+}
+
+    formEl.addEventListener('input', formHandler)
--- js/lv-submenu.js
+++ js/lv-submenu.js
@@ -2,15 +2,79 @@
 */
 
 $(document).ready(function() {
-    $("#js-cat").click(function() {
-        var isHidden = $(".sidenav__submenu").is(":hidden");
+    $("#js-drop1").click(function() {
+        var isHidden = $("#menu1").is(":hidden");
         if (isHidden) {
-            $('.sidenav__submenu').slideDown();
+            $("#menu1").slideDown();
         } else {
-            $('.sidenav__submenu').slideUp();
+            $("#menu1").slideUp();
         }
     })
+});
 
+$(document).ready(function() {
+    $("#js-drop2").click(function() {
+        var isHidden = $("#menu2").is(":hidden");
+        if (isHidden) {
+            $("#menu2").slideDown();
+        } else {
+            $("#menu2").slideUp();
+        }
+    })
+});
 
+$(document).ready(function() {
+    $("#js-drop2").click(function() {
+        var isHidden = $("#menu2").is(":hidden");
+        if (isHidden) {
+            $("#menu2").slideDown();
+        } else {
+            $("#menu2").slideUp();
+        }
+    })
+});
+
+$(document).ready(function() {
+    $("#js-drop3").click(function() {
+        var isHidden = $("#menu3").is(":hidden");
+        if (isHidden) {
+            $("#menu3").slideDown();
+        } else {
+            $("#menu3").slideUp();
+        }
+    })
+});
+
+$(document).ready(function() {
+    $("#js-drop4").click(function() {
+        var isHidden = $("#menu4").is(":hidden");
+        if (isHidden) {
+            $("#menu4").slideDown();
+        } else {
+            $("#menu4").slideUp();
+        }
+    })
+});
+
+$(document).ready(function() {
+    $("#js-drop5").click(function() {
+        var isHidden = $("#menu5").is(":hidden");
+        if (isHidden) {
+            $("#menu5").slideDown();
+        } else {
+            $("#menu5").slideUp();
+        }
+    })
+});
+
+$(document).ready(function() {
+    $("#js-drop6").click(function() {
+        var isHidden = $("#menu6").is(":hidden");
+        if (isHidden) {
+            $("#menu6").slideDown();
+        } else {
+            $("#menu6").slideUp();
+        }
+    })
 });
 
